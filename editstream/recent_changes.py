41003546c1873081f47b74aa8634fccee962a5b1
tim <tim@87ae43fb-6b68-496d-bb1c-0f2e52d60556>
2010-10-25 00:43:32 +0000

    try again
    git-svn-id: https://cobihome.external.cluenet.org:8443/svn/cluebotng@30 87ae43fb-6b68-496d-bb1c-0f2e52d60556

---
+++ editstream/recent_changes.py
@@ -0,0 +1,86 @@
+from socket import socket,AF_INET,SOCK_STREAM
+from urllib2 import urlopen
+from re import compile
+def get_whitelist():
+       
+        whitelist = urlopen('http://en.org/w/index.php?title=Wikipedia:Huggle/Whitelist&action=raw&templates=expand').read().split('\n')[2:-1]
+        return whitelist
+        
+class bot:
+        def __init__(self):
+                self.regexps = compile(r'\:.*? PRIVMSG (?P<channel>.*?) \:\x0314\[\[\x0307(?P<title>.*?)\x0314\]\]\x034 .*?\x0302http://.*?/w/index\.php\?diff\=(?P<diff>[0-9]*)\&oldid\=(?P<oldid>[0-9]*)\x03 \x035\*\x03 \x0303(?P<user>.*?)\x03 \x035\*\x03 \((?P<diffsize>[\+\-][0-9]*)\) \x0310(?P<comment>.*?)\x03')
+                self.connect()
+                self.whitelist = get_whitelist()
+             
+                #self.flog = log()a
+                        #self.loadregexp
+        def connect(self):
+                        server = 'irc.wikimedia.org'
+                        port = 6667
+                       
+                        nickname = 'DASHBotAV'
+                        rc = socket(AF_INET, SOCK_STREAM)
+                        rc.connect((str(server), int(port)))
+                        rc.recv(4096)
+                        nick = '%s' %(nickname)
+                        rc.send('NICK %s\r\n' %nick)
+                        
+                        rc.send('USER %s %s %s :%s\r\n' %(nick, nick, nick, nick))
+                        rc.send('JOIN %s\r\n' %'#en.wikipedia')
+                        self.rc = rc
+        def run(self):
+        		
+                    
+                     now=10
+                     while True:
+
+                     	try:
+                     		d = self.rc.recv(4096)
+                     	except KeyboardInterrupt:
+                     		print '>'*5,'QUIT','<'*5
+                     		quit()
+                     	except:
+                     		del self.rc
+                     		self.connect()
+                     		continue
+                     	#print d
+                     	d = d.decode('utf-8', 'replace')
+                     	if 'PING' in d:
+                                        self.rc.send('PONG ' + d.split()[1] + '\r\n')
+                        elif d == '':
+                        	try:
+                        		self.rc.send('QUIT\r\n')
+                        		self.rc.close()
+                        		del self.rc
+                        	except:
+                        		#socket error
+                        		pass
+                        	self.connect()                        	
+                        		
+                     	else: 
+                                        self.handlemsg(d)
+       
+        def handlemsg(self,d):
+                        if not self.regexps.match(d):
+                        
+                                return
+                        
+                        msg = self.regexps.match(d).groupdict()
+                     #   print msg
+                        del d
+                        if msg['title'] == 'User:Tim1357/Bot settings.css':
+                                self.reloadregexps()
+                        if msg['title'] == 'User:Tim1357/Exclusion list.css':
+                                self.loadignore()
+                        if msg['title'] == 'User:DASHBot/Vandalism/Die':
+                                quit(msg['user']+' killed me...')
+                        
+                        if msg['user'].encode('utf-8') in self.whitelist:
+                                return
+                        
+                        
+                    
+                        
+                        print msg['diff']+'\t\t'+msg['title']
+       
+if __name__=='__main__':bot().run()
