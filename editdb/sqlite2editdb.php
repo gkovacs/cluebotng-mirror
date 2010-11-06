9c44f6a77d28f567873fa2f22d735fcd22ef38d0
cobi <cobi@87ae43fb-6b68-496d-bb1c-0f2e52d60556>
2010-11-06 04:36:25 +0000

    fix ...
    git-svn-id: https://cobihome.external.cluenet.org:8443/svn/cluebotng@96 87ae43fb-6b68-496d-bb1c-0f2e52d60556

--- editdb/sqlite2editdb.php
+++ editdb/sqlite2editdb.php
@@ -3,7 +3,7 @@
 		return stripcslashes( substr( $string, 1, -1 ) );
 	}
 	
-	function insertIntoDatabase( $source, $isVandalism, $data ) {
+/*	function insertIntoDatabase( $source, $isVandalism, $data ) {
 		global $mysql;
 		$queryStr = <<<'EOQ'
 			INSERT INTO `editset` (
@@ -76,6 +76,71 @@ EOQ;
 		
 		if( !$query->execute() )
 			echo 'Something went wrong.  ' . $query->error . "\n";
+	}*/
+
+	function insertIntoDatabase( $source, $isVandalism, $data ) {
+		$query = 'INSERT INTO `editset` (
+				`edittype`,
+				`editid`,
+				`comment`,
+				`user`,
+				`user_edit_count`,
+				`user_distinct_pages`,
+				`user_warns`,
+				`prev_user`,
+				`user_reg_time`,
+				`common_page_made_time`,
+				`common_title`,
+				`common_namespace`,
+				`common_creator`,
+				`common_num_recent_edits`,
+				`common_num_recent_reversions`,
+				`current_minor`,
+				`current_timestamp`,
+				`current_text`,
+				`previous_timestamp`,
+				`previous_text`,
+				`isvandalism`,
+				`isactive`,
+				`source`,
+				`reviewers`,
+				`reviewers_agreeing`
+			) VALUES (';
+		$escaped = Array(
+			$data->EditType,
+			$data->EditID,
+			$data->comment,
+			$data->user,
+			$data->user_edit_count,
+			$data->user_distinct_pages,
+			$data->user_warns,
+			$data->prev_user,
+			$data->user_reg_time,
+			$data->common->page_made_time,
+			$data->common->title,
+			$data->common->namespace,
+			$data->common->creator,
+			$data->common->num_recent_edits,
+			$data->common->num_recent_reversions,
+			$data->current->minor == 'true' ? 1 : 0,
+			$data->current->timestamp,
+			$data->current->text,
+			$data->previous->timestamp,
+			$data->previous->text,
+			$isVandalism ? 1 : 0,
+			1,
+			$source
+		);
+		foreach( $escaped as $key => &$value )
+			if( $key == 8 or $key == 9 or $key == 16 or $key == 18 )
+				$value = 'FROM_UNIXTIME( \'' . mysql_real_escape_string( $value ) . '\' )';
+			else
+				$value = '\'' . mysql_real_escape_string( $value ) . '\'';
+		
+		$query .= implode( ',', $escaped );
+		$query .= ', NULL, NULL )';
+		if( !mysql_query( $query ) )
+			echo 'An error occurred: ' . mysql_error() . "\n";
 	}
 	
 	function parseEntry( $entry ) {
@@ -89,7 +154,12 @@ EOQ;
 
 	$sqlitedb = '/home/cobi/alldataset.sqlite';
 	
-	$mysql = new MySQLi( '192.168.2.11', 'root', '********', 'cbng_editdb_master' );
+	//$mysql = new MySQLi( '192.168.2.11', 'root', '********', 'cbng_editdb_master' );
+	$mysql = mysql_connect( 'localhost', 'cbngdb', '' );
+	if( !$mysql )
+		die( 'Error: ' . mysql_error() );
+	if( !mysql_select_db( $mysql, 'cbng_editdb_master' ) )
+		die( 'Error; ' . mysql_error() );
 	$sqlite = new SQLite3( $sqlitedb );
 	$query = $sqlite->query( 'SELECT EditID, EditXML, IsVandalism, EditSource FROM dataset' );
 	
