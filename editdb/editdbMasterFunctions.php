a2b2b41fa9dd12f4c1e3a927c67d7b7db3809a17
cobi <cobi@87ae43fb-6b68-496d-bb1c-0f2e52d60556>
2010-11-14 03:39:52 +0000

    More.
    git-svn-id: https://cobihome.external.cluenet.org:8443/svn/cluebotng@126 87ae43fb-6b68-496d-bb1c-0f2e52d60556

--- editdb/editdbMasterFunctions.php
+++ editdb/editdbMasterFunctions.php
@@ -23,17 +23,99 @@
 		
 		return $mysql;
 	}
-
-	function insertEdit( $id, $isVand, $isActive, $reviewers, $reviewers_agreeing, $source ) {
+	
+	function getMasterData( $id, $misc = false ) {
 		$mysql = getMasterMySQL();
 		
-		$data = oldData( $id );
+		$ret = mysql_fetch_assoc(
+			mysql_query(
+				'SELECT `edittype`,
+				`editid`,
+				`comment`,
+				`user`,
+				`user_edit_count`,
+				`user_distinct_pages`,
+				`user_warns`,
+				`prev_user`,
+				UNIX_TIMESTAMP( `user_reg_time` ) AS `user_reg_time_unix`,
+				UNIX_TIMESTAMP( `common_page_made_time` ) AS `common_page_made_time_unix`,
+				`common_title`,
+				`common_namespace`,
+				`common_creator`,
+				`common_num_recent_edits`,
+				`common_num_recent_reversions`,
+				`current_minor`,
+				UNIX_TIMESTAMP( `current_timestamp` ) AS `current_timestamp_unix`,
+				`current_text`,
+				UNIX_TIMESTAMP( `previous_timestamp` ) AS `previous_timestamp_unix`,
+				`previous_text`,
+				`isvandalism`,
+				`isactive`,
+				`source`,
+				UNIX_TIMESTAMP( `updated` ) AS `updated_unix`,
+				`reviewers`,
+				`reviewers_agreeing` FROM `editset` WHERE `editid` = \'' . mysql_real_escape_string( $id ) . '\''
+			)
+		);
+		
+		$data = Array(
+			'EditType' => $ret[ 'edittype' ],
+			'EditID' => $ret[ 'editid' ],
+			'comment' => $ret[ 'comment' ],
+			'user' => $ret[ 'user' ],
+			'user_edit_count' => $ret[ 'user_edit_count' ],
+			'user_distinct_pages' => $ret[ 'user_distinct_pages' ],
+			'user_warns' => $ret[ 'user_warns' ],
+			'prev_user' => $ret[ 'prev_user' ],
+			'user_reg_time' => $ret[ 'user_reg_time_unix' ],
+			'common' => Array(
+				'page_made_time' => $ret[ 'common_page_made_time_unix' ],
+				'title' => $ret[ 'common_title' ],
+				'namespace' => $ret[ 'common_namespace' ],
+				'creator' => $ret[ 'common_creator' ],
+				'num_recent_edits' => $ret[ 'common_num_recent_edits' ],
+				'num_recent_reversions' => $ret[ 'common_num_recent_reversions' ]
+			),
+			'current' => Array(
+				'minor' => $ret[ 'current_minor' ] ? 'true' : 'false',
+				'timestamp' => $ret[ 'current_timestamp_unix' ],
+				'text' => $ret[ 'current_text' ]
+			),
+			'previous' => Array(
+				'timestamp' => $ret[ 'previous_timestamp_unix' ],
+				'text' => $ret[ 'previous_text' ]
+			)
+		);
+		
+		if( $misc ) {
+			$data[ 'EditDB' ] = Array(
+				'isActive' => $ret[ 'isactive' ] ? 'true' : 'false',
+				'source' => $ret[ 'source' ],
+				'lastUpdated' => $ret[ 'updated_unix' ]
+			);
+			
+			$data[ 'isVandalism' ] = $ret[ 'isvandalism' ] ? 'true' : 'false';
+			
+			$data[ 'ReviewInterface' ] = Array(
+				'reviewers' => $ret[ 'reviewers' ],
+				'reviewers_agreeing' => $ret[ 'reviewers_agreeing' ]
+			);
+		}
+		
+		return $data;
+	}
+
+	function insertEdit( $id, $isVand, $isActive, $reviewers, $reviewers_agreeing, $source, $data = null ) {
+		$mysql = getMasterMySQL();
+		
+		if( $data === null )
+			$data = oldData( $id );
 		if( $data === false ) {
 			echo 'Bad revid.  Not inserted.';
 			return false;
 		}
 		
-		$query = 'INSERT INTO `editset` (
+		$query = 'REPLACE INTO `editset` (
 				`edittype`,
 				`editid`,
 				`comment`,
