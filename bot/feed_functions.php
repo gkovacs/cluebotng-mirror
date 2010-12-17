20d9f4ddc2a1b8ad3ce854ea639cfb3de9240aef
cobi <cobi@87ae43fb-6b68-496d-bb1c-0f2e52d60556>
2010-12-17 01:58:34 +0000

    Fix.
    git-svn-id: https://cobihome.external.cluenet.org:8443/svn/cluebotng@177 87ae43fb-6b68-496d-bb1c-0f2e52d60556

--- bot/feed_functions.php
+++ bot/feed_functions.php
@@ -28,7 +28,7 @@
 		
 		public static function bail( $change, $why = '', $score = 'N/A', $reverted = false ) {
 			$udp = fsockopen( 'udp://' . Config::$udphost, Config::$udpport );
-			fwrite( $udp, $change[ 'rawline' ] . "\003 # " . $score . ' # ' . $why . ' # ' . $reverted );
+			fwrite( $udp, $change[ 'rawline' ] . "\003 # " . $score . ' # ' . $why . ' # ' . ( $reverted ? 'Reverted' : 'Not reverted' ) );
 			fclose( $udp );
 		}
 		
